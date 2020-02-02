7fb110275de989aa2e7789aa2fdee9b5d3fdc7cf
7Romero <59968467+7Romero@users.noreply.github.com>
2020-02-02 13:54:46 +0200

    Update connect_bd.py

--- BD/connect_bd.py
+++ BD/connect_bd.py
@@ -2,10 +2,10 @@ import mysql.connector
 
 def connectBD():
     mydb = mysql.connector.connect(
-        host="149.202.88.165",
-        user="gs47840",
-        passwd="SYeCIyl9fgvB",
-        database="gs47840"
+        host="",
+        user="",
+        passwd="",
+        database=""
         )
 
     return mydb
